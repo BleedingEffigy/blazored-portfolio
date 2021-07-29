7abc9a3270139f402c9a104e2cbe5a5b77882a0f
phantiahazd <>
2021-07-29 10:12:35 -0700

    Each project now has a modal

--- Shared/Project.razor.cs
+++ Shared/Project.razor.cs
@@ -9,7 +9,22 @@ namespace BlazoredPortfolio.Shared
        public string title {get; set;}
        public string sourceLink {get; set;}
        public string siteLink {get; set;}
-       public string smartContract {get; set;} = "";
+       public string photoStructure {get; set; } = "";
+       public ImageTag[] ImgStructure {get; set; } = new ImageTag[]{};
+       public string smartContract {get; set; } = "";
        public bool visible {get; set;}
+    }
+    public struct ImageTag
+    {
+        public int size {get; set;}
+        public string imgRatio {get; set;}
+        public string imgUrl {get; set;}
+        public ImageTag(int size, string imgRatio, string imgUrl)
+    {
+        this.size = size;
+        this.imgRatio = imgRatio;
+        this.imgUrl = imgUrl;
+    }
+           
     }
 }

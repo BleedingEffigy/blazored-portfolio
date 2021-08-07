0145035c3e7f2e10f217abd98b3c767d1e0b06c3
phantiahazd <>
2021-08-07 15:35:32 -0700

    Made changes for production deployment

--- Startup.cs
+++ Startup.cs
@@ -52,7 +52,6 @@ namespace BlazoredPortfolio
             });
             app.UseAuthentication();
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
